30aad9f2d8fe35615ed1064d2f0bb9c6927cb163
FYATMM <fengyi.jump.out@gmail.com>
2019-05-30 13:19:30 +0800

    删除Branch多余注释.提取左右等按键displayCurrentRow

--- RefactoringCode/RentAWheel/RentAWheel/FrmBranch.cs
+++ RefactoringCode/RentAWheel/RentAWheel/FrmBranch.cs
@@ -25,11 +25,6 @@ namespace RentAWheel
         {
             string connectionString = "Data Source=(local);Initial Catalog=RENTAWHEELS;Integrated Security=True";
             SqlConnection connection = new SqlConnection(connectionString);
-            ////SqlConnection connection = new SqlConnection(
-                //"Data Source=TESLATEAM;" +
-                //"Initial Catalog=RENTAWHEELS;" +
-                //"User ID=RENTAWHEELS_LOGIN;Password=RENTAWHEELS_PASSWORD_123");
-                ////);
             SqlCommand command = new SqlCommand();
             DataSet dsBranch = new DataSet();
             SqlDataAdapter adapter = new SqlDataAdapter();
@@ -64,14 +59,19 @@ namespace RentAWheel
             }
         }
 
+        private void DisplayCurrentRow()
+        {
+            DataRow drRow = dtBranch.Rows[currentRowIndex];
+            txtId.Text = drRow["BranchId"].ToString();
+            BranchName.Text = drRow["Name"].ToString();
+        }
+
         private void btnRight_Click(object sender, EventArgs e)
         {
             if (dtBranch.Rows.Count > currentRowIndex + 1)
             {
                 currentRowIndex++;
-                DataRow drRow = dtBranch.Rows[currentRowIndex];
-                txtId.Text = drRow["BranchId"].ToString();
-                BranchName.Text = drRow["Name"].ToString();
+                DisplayCurrentRow();
             }
         }
 
@@ -80,9 +80,7 @@ namespace RentAWheel
             if (currentRowIndex - 1 >= 0 & dtBranch.Rows.Count > 0)
             {
                 currentRowIndex--;
-                DataRow drRow = dtBranch.Rows[currentRowIndex];
-                txtId.Text = drRow["BranchId"].ToString();
-                BranchName.Text = drRow["Name"].ToString();
+                DisplayCurrentRow();
             }
         }
 
@@ -91,9 +89,7 @@ namespace RentAWheel
             if (dtBranch.Rows.Count > 0)
             {
                 currentRowIndex = 0;
-                DataRow drRow = dtBranch.Rows[currentRowIndex];
-                txtId.Text = drRow["BranchId"].ToString();
-                BranchName.Text = drRow["Name"].ToString();
+                DisplayCurrentRow();
             }
         }
 
@@ -102,9 +98,7 @@ namespace RentAWheel
             if (dtBranch.Rows.Count > 0)
             {
                 currentRowIndex = dtBranch.Rows.Count - 1;
-                DataRow drRow = dtBranch.Rows[currentRowIndex];
-                txtId.Text = drRow["BranchId"].ToString();
-                BranchName.Text = drRow["Name"].ToString();
+                DisplayCurrentRow();
             }
         }
 
@@ -113,24 +107,18 @@ namespace RentAWheel
             string strSql;
             string connectionString = "Data Source=(local);Initial Catalog=RENTAWHEELS;Integrated Security=True";
             SqlConnection connection = new SqlConnection(connectionString);
-            //SqlConnection connection = new SqlConnection(
-            //   "Data Source=TESLATEAM;" +
-            //   "Initial Catalog=RENTAWHEELS;" +
-            //   "User ID=RENTAWHEELS_LOGIN;Password=RENTAWHEELS_PASSWORD_123");
             SqlCommand command = new SqlCommand();
             if (txtId.Text.Equals(""))
             {
                 //Create Sql String with parameter @SelectedLP
-                strSql = "Insert Into Branch (Name) " +
-                "Values(@Name)";
+                strSql = "Insert Into Branch (Name) " + "Values(@Name)";
                 //add parameter name
                 command.Parameters.AddWithValue("@Name", BranchName.Text);
             }
             else
             {
                 //Create Sql String with parameter @SelectedLP
-                strSql = "Update Branch  Set Name = @Name " +
-                "Where BranchId = @Id";
+                strSql = "Update Branch  Set Name = @Name " + "Where BranchId = @Id";
                 //add parameter name
                 command.Parameters.AddWithValue("@Name", BranchName.Text);
                 //add parameter Id
@@ -138,21 +126,15 @@ namespace RentAWheel
             }
             try
             {
-                //open connection
                 connection.Open();
-                //Set connection to command
                 command.Connection = connection;
-                //set Sql string to command object
                 command.CommandText = strSql;
-                //exexute command
-                command.ExecuteNonQuery();
-                //close connection
+                command.ExecuteNonQuery();            
                 connection.Close();
             }
             catch
             {
-                MessageBox.Show("A problem occurred and the application cannot recover! " +
-                "Please contact the technical support.");
+                MessageBox.Show("A problem occurred and the application cannot recover! Please contact the technical support.");
             }
             BranchMaintenance_Load(null, null);
         }
@@ -161,33 +143,21 @@ namespace RentAWheel
         {
             string connectionString = "Data Source=(local);Initial Catalog=RENTAWHEELS;Integrated Security=True";
             SqlConnection connection = new SqlConnection(connectionString);
-            //SqlConnection connection = new SqlConnection(
-            //  "Data Source=TESLATEAM;" +
-            //  "Initial Catalog=RENTAWHEELS;" +
-            //  "User ID=RENTAWHEELS_LOGIN;Password=RENTAWHEELS_PASSWORD_123");
             SqlCommand command = new SqlCommand();
             //add parameter name
-            string strSql = "Delete Branch " +
-            "Where BranchId = @Id";
-            command.Parameters.AddWithValue(
-                "@Id", Convert.ToInt16(txtId.Text));
+            string strSql = "Delete Branch " +  "Where BranchId = @Id";
+            command.Parameters.AddWithValue("@Id", Convert.ToInt16(txtId.Text));
             try
             {
-                //open connection
                 connection.Open();
-                //Set connection to command
                 command.Connection = connection;
-                //set Sql string to command object
                 command.CommandText = strSql;
-                //exexute command
                 command.ExecuteNonQuery();
-                //close connection
                 connection.Close();
             }
             catch
             {
-                MessageBox.Show("A problem occurred and the application cannot recover! " +
-                "Please contact the technical support.");
+                MessageBox.Show("A problem occurred and the application cannot recover! " + "Please contact the technical support.");
             }
             BranchMaintenance_Load(null, null);
         }
