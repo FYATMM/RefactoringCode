c9edfd3d9e83725afe371217714b1dddc2a882b4
FYATMM <fengyi.jump.out@gmail.com>
2019-05-31 13:02:11 +0800

    数据类替换行v3.1将业务逻辑移入数据类中，比如条件判断

--- RefactoringCode/用数据类取代行/Account.cs
+++ RefactoringCode/用数据类取代行/Account.cs
@@ -3,12 +3,17 @@
 namespace DataBaseCmd2Class
 {
    public class Account
-    {
+   {
+       private decimal balance;
         public string Number { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
-        public decimal Balance { get; set; }
-        public bool Blocked { get; set; }
 
+        public decimal Balance
+        {
+            get => Blocked ? 0 : balance;
+            set => balance = value;
+        }
+        public bool Blocked { get; set; }
     }
 }
--- RefactoringCode/用数据类取代行/AccountView.cs
+++ RefactoringCode/用数据类取代行/AccountView.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.Globalization;
 using System.Windows.Forms;
 
 /// <summary>
@@ -8,8 +9,12 @@ using System.Windows.Forms;
 /// 数据访问 和 其它API 常常在通信层中使用一个**行**的范式来检索各种数据
 /// 比如System.Data.DataRow类
 ///
-/// 用数据类替代：
+/// 用数据类替代：V2.0
 /// 每个属性 对应 行中的项
+///
+/// 将表示层与业务层分离：v3.0
+/// 将业务逻辑移到之前定义的数据类中
+/// 比如条件判断
 /// </summary>
 namespace DataBaseCmd2Class
 {
@@ -24,18 +29,11 @@ namespace DataBaseCmd2Class
         }
         public void viewAccountDetails_Click(object sender, System.EventArgs e)
         {
-            Account account =  GetAccount(Number.Text);
+            Account account = GetAccount(Number.Text);
             //Fill controls on the form
             Name.Text = account.Name; //Name.Text = accountRow["Name"].ToString();
             Type.Text = account.Type; //Type.Text = accountRow["Type"].ToString();
-            if (!account.Blocked) ////if (!Convert.ToBoolean(accountRow["Blocked"]))
-            {
-                Balance.Text = account.Balance.ToString(); ////Balance.Text = accountRow["Balance"].ToString();
-            }
-            else
-            {
-                Balance.Text = "Blocked";
-            }
+            Balance.Text = account.Balance.ToString(CultureInfo.InvariantCulture); ////Balance.Text = accountRow["Balance"].ToString();
         }
 
         private Account GetAccount(string number)
