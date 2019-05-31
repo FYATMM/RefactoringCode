1efc650989b0b19c1a7abf89b3738dd162f01063
FYATMM <fengyi.jump.out@gmail.com>
2019-05-31 11:45:48 +0800

    数据类替代行v2.0，创建account类，其属性为数据行的每一项，把数据库内容赋值给account对象，调用其属性

---
+++ RefactoringCode/用数据类取代行/Account.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Principal;
+
+namespace DataBaseCmd2Class
+{
+   public class Account
+    {
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public decimal Balance { get; set; }
+        public bool Blocked { get; set; }
+
+    }
+}
--- RefactoringCode/用数据类取代行/AccountView.cs
+++ RefactoringCode/用数据类取代行/AccountView.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
 /// <summary>
 /// 两层结构，表示层直接和数据库通讯
 /// 数据访问 和 其它API 常常在通信层中使用一个**行**的范式来检索各种数据
@@ -27,40 +22,53 @@ namespace DataBaseCmd2Class
         {
             InitializeComponent();
         }
-            public void viewAccountDetails_Click(object sender, System.EventArgs e)
+        public void viewAccountDetails_Click(object sender, System.EventArgs e)
+        {
+            Account account =  GetAccount(Number.Text);
+            //Fill controls on the form
+            Name.Text = account.Name; //Name.Text = accountRow["Name"].ToString();
+            Type.Text = account.Type; //Type.Text = accountRow["Type"].ToString();
+            if (!account.Blocked) ////if (!Convert.ToBoolean(accountRow["Blocked"]))
             {
-                IDbConnection connection = new SqlConnection(connectionString);
-                IDbDataAdapter adapter = new SqlDataAdapter();
-
-                DataSet accountDataSet = new DataSet();
-
-                IDbCommand command = new SqlCommand();
-                string sql = "Select * from AccountsDemo where Number = " + Number.Text;
-
-                connection.Open();
-                command.Connection = connection;
-                command.CommandText = sql;
-                adapter.SelectCommand = command;
-                adapter.Fill(accountDataSet);
-                connection.Close();
-
-                accountTable = accountDataSet.Tables[0];
-                DataRow accountRow = accountTable.Rows[0];
-                //Fill controls on the form
-                Name.Text = accountRow["Name"].ToString();
-                Type.Text = accountRow["Type"].ToString();
-                if(!Convert.ToBoolean(accountRow["Blocked"]))
-                {
-                    Balance.Text = accountRow["Balance"].ToString();
-                }
-                else
-                {
-                    Balance.Text = "Blocked";
-                }
+                Balance.Text = account.Balance.ToString(); ////Balance.Text = accountRow["Balance"].ToString();
             }
-
+            else
+            {
+                Balance.Text = "Blocked";
             }
+        }
 
+        private Account GetAccount(string number)
+        {
+            IDbConnection connection = new SqlConnection(connectionString);
+            IDbDataAdapter adapter = new SqlDataAdapter();
+
+            DataSet accountDataSet = new DataSet();
+
+            IDbCommand command = new SqlCommand();
+            string sql = "Select * from AccountsDemo where Number = " + Number.Text;
+
+            connection.Open();
+            command.Connection = connection;
+            command.CommandText = sql;
+            adapter.SelectCommand = command;
+            adapter.Fill(accountDataSet);
+            connection.Close();
+
+            accountTable = accountDataSet.Tables[0];
+            DataRow accountRow = accountTable.Rows[0];
+            ////把数据库数据导入到account对象
+            Account account = new Account();
+            account.Number = accountRow["Number"].ToString();
+            account.Name = accountRow["Name"].ToString();
+            account.Type = accountRow["Type"].ToString();
+            account.Balance = Convert.ToDecimal(accountRow["Balance"]);
+            account.Blocked = Convert.ToBoolean(accountRow["Blocked"]);
+
+            return account;
+        }
     }
-    
+
+}
+
 
